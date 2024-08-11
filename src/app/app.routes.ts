e405e735947b715a45ab3ad16a52184a8e078aed
Alexis <alexis.robotiik@hotmail.fr>
2024-08-11 17:48:59 +0200

    style (landing) : responsive sur page d'accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
     {
         path: '', component: LandingPageComponent
+    },
+    {
+        path: 'projects', component: ProjectsComponent
     },
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
