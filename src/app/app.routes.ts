45bbcd04a3410dc34bbe7d9c163bfe22736effe2
Alexis <alexis.robotiik@hotmail.fr>
2024-08-11 17:55:06 +0200

    feat (navbar) : création autres pages vierges

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {
@@ -8,5 +9,8 @@ export const routes: Routes = [
     },
     {
         path: 'projects', component: ProjectsComponent
+    },
+    {
+        path: 'contact', component: ContactComponent
     },
 ];
