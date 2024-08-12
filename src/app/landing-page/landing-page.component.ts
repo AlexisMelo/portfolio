b163b490442bba63ea209f59c961273b3d09765e
Alexis <alexis.robotiik@hotmail.fr>
2024-08-12 13:17:17 +0200

    style (landing) : navbar soulignage stylé et landing animation sur melo

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { TimelineComponent } from './timeline/timeline.component';
 
 @Component({
@@ -8,6 +8,54 @@ import { TimelineComponent } from './timeline/timeline.component';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit {
+  /**
+   * HTMLElement of main title element
+   */
+  @ViewChild("title") title?: ElementRef;
 
+  /**
+   * HTMLElement of main title element
+   */
+  @ViewChild("placeholder") placeholder?: ElementRef;
+
+  /**
+   * Content of main title
+   */
+  public titleContent: string = 'MELO.';
+
+  /**
+   * Timer reference to unsubscribe once finished
+   */
+  private typewriterInterval?: number;
+
+  /**
+   * Time for the typing effect in milliseconds
+   */
+  private typingDuration = 500;
+  
+  /**
+   * Adds letter to position i at the title element
+   * @param i 
+   */
+  private typewrite() {
+    let i = 0;
+
+    this.typewriterInterval = window.setInterval(() => {
+      if (i >= this.titleContent.length || !this.title || !this.placeholder) {
+        clearInterval(this.typewriterInterval);
+        return;
+      };
+      this.title.nativeElement.innerHTML += this.titleContent.charAt(i);    
+      this.placeholder.nativeElement.innerHTML = this.placeholder.nativeElement.innerHTML.substring(1);
+      i++;
+    }, this.typingDuration / this.titleContent.length);
+  }
+
+  /**
+   * AfterViewInit implementation
+   */
+  ngAfterViewInit(): void {
+    this.typewrite();
+  }
 }
