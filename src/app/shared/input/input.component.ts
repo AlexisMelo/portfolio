17327f5430010aa206b8c61746901ae6fe07473b
EMEA\mea2mo <Alexis.MeloDaSilva@fr.bosch.com>
2024-08-30 11:22:12 +0200

    feat (contact) : améliorations page de contact

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
