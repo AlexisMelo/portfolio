e9b808785849b25712494fbf8eea7f3108277be1
EMEA\mea2mo <Alexis.MeloDaSilva@fr.bosch.com>
2024-09-30 14:36:04 +0200

    fix (navbar) : collapsible navbar sur petits écrans

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,26 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  inject,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ThemeService } from '../shared/theme.service';
+import { NgTemplateOutlet } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule, RouterLink, RouterLinkActive],
+  imports: [MatIconModule, RouterLink, RouterLinkActive, NgTemplateOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   /**
-   * Constructeur
+   * Est-ce que le menu est ouvert
    */
-  constructor(public themeService: ThemeService) {}
+  @HostBinding('class.menu-opened') public menuOpened: boolean = true;
 
   /**
-   * Active / Désactive le dark mode
+   * Gestion du thème
+   */
+  public themeService = inject(ThemeService);
+
+  /**
+   * Gestion des routes
+   */
+  private router = inject(Router);
+
+  /**
+   * Souscription aux changements de routes
+   */
+  private routerSubscription?: Subscription;
+
+  /**
+   * Implémentation de OnInit
    */
 
+  ngOnInit() {
+    this.routerSubscription = this.router.events.subscribe({
+      next: () => (this.menuOpened = false),
+    });
+  }
+
+  /**
+   * Implémentation de OnDestroy
+   */
+  ngOnDestroy() {
+    this.routerSubscription?.unsubscribe();
+  }
+
+  /**
+   * Change le thème de l'app
+   */
   public toggleDarkMode() {
     this.themeService.toggle();
+  }
+
+  /**
+   * Ouvre le menu
+   */
+  public toggleMenu() {
+    this.menuOpened = !this.menuOpened;
   }
 }
