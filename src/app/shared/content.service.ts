efbcead2466f041d628cdf242cfa0c4fa1d3bb98
EMEA\mea2mo <Alexis.MeloDaSilva@fr.bosch.com>
2025-01-30 10:26:01 +0100

    fix (landing): changement image et description

--- src/app/shared/content.service.ts
+++ src/app/shared/content.service.ts
@@ -13,7 +13,14 @@ export class ContentService {
    * Description de mon activité
    */
   get description() {
-    return "Je suis Alexis Melo da Silva, développeur fullstack basé à Caen, en France. Je met mes compétences en développement web au service de la transformation digitale des entreprises et au déploiement de l'i4.0.";
+    // return "Je suis Alexis Melo da Silva, développeur fullstack basé à Caen, en France. Je met mes compétences en développement web au service de la transformation digitale des entreprises et au déploiement de l'i4.0.";
+    return `Fort de 3 ans d’expérience en 
+développement logiciel et web au sein
+d’une grande entreprise, je souhaite 
+désormais exercer ma passion sous le 
+statut de freelance. 
+Aimant aussi bien participer à
+l’élaboration d’interfaces que réfléchir sur les problématiques liées à la gestion de la donnée, j’aspire chaque jour à devenir un développeur un peu plus complet.`;
   }
 
   /**
