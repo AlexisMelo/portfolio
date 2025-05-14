b7f19bcfd16aeade08b27d1ce311c2d7227cd9e0
Alexis Melo <Alexis.Robotiik@hotmail.fr>
2025-05-14 10:53:46 +0200

    style (app): added fade in animation to all routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {
+  trigger,
+  transition,
+  group,
+  query,
+  style,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +17,28 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [
+    trigger('routeFade', [
+      transition('* <=> *', [
+        group([
+          query(
+            ':enter',
+            [
+              style({ opacity: 0 }),
+              animate('400ms ease-in', style({ opacity: 1 })),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
-export class AppComponent {}
+export class AppComponent {
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet.isActivated
+      ? outlet.activatedRouteData['animation'] ||
+          outlet.activatedRoute?.component?.name
+      : null;
+  }
+}
