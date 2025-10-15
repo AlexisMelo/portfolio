27e0380d3ef196bd0e87b732a905fe5d949c48e3
Alexis Melo <Alexis.Robotiik@hotmail.fr>
2025-10-15 10:38:54 +0200

    feat (keep-learning): displaying currenctly learning skills

--- src/app/skills/keep-learning/keep-learning.component.ts
+++ src/app/skills/keep-learning/keep-learning.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { GridItemDirective } from 'src/app/shared/grid/grid-item.directive';
+import { Skill } from '../skill.model';
+import { LearningPipe } from '../learning.pipe';
+import { SupabaseService } from 'src/app/shared/supabase.service';
 
 @Component({
   selector: 'app-keep-learning',
@@ -8,5 +11,30 @@ import { GridItemDirective } from 'src/app/shared/grid/grid-item.directive';
   templateUrl: './keep-learning.component.html',
   styleUrl: './keep-learning.component.scss',
   hostDirectives: [GridItemDirective],
+  providers: [LearningPipe],
 })
-export class KeepLearningComponent {}
+export class KeepLearningComponent {
+  /**
+   * Handle database data
+   */
+  private supabaseService = inject(SupabaseService);
+
+  /**
+   * Skills to display
+   */
+  public skills = signal<Array<Skill>>([]);
+
+  /**
+   * Filter skills based on loved ones
+   */
+  private learningPipe = inject(LearningPipe);
+
+  /**
+   * Constructor
+   */
+  constructor() {
+    this.supabaseService
+      .getSkills()
+      .then(skills => this.skills.set(this.learningPipe.transform(skills)));
+  }
+}
